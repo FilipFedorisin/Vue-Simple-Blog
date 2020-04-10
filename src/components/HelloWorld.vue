2189bacd7d04514b3c79e1e8a632a0c4e9e040ac
FilipFedorisin <fedorisinfilio@gmail.com>
2020-04-10 17:30:13 +0200

    d

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="hello"></div>
+  <div>
+    <h1>PENI
+      
+      S
+      
+</h1>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-};
+export default {};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
