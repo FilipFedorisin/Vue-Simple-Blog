addf0cf5a2fb0337084136cd15c71bb950498a01
FilipFedorisin <fedorisinfilio@gmail.com>
2020-04-10 23:09:02 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <head>
+      <titl>Blog</titl>
+    </head>
+    <addBlog></addBlog>
+  </div>
 </template>
 
 <script>
+import addBlog from './components/addBlog.vue';
 export default {
+  components: {
+    addBlog,
+  },
   data() {
-  return      {
-  }
-}
+    return {};
+  },
+  created() {
+    document.title = 'Blog';
+  },
+};
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
